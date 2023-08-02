6afb3057146894fa1911a581c9c02ca44addf55f
Kartik Narang <narangk007@gmail.com>
2023-08-02 23:51:45 +0530

    Handling Requests

--- my-first-app.js
+++ my-first-app.js
@@ -1,5 +1,40 @@
 const http = require('http');
+
 const server = http.createServer((req,res)=>{
-    console.log("Kartik Narang");
-});
-server.listen(4000);
+    const url = req.url;
+    if(url == '/home'){
+        res.write('<html>');
+        res.write('<head><title>Node JS Response</title></head>');
+        res.write('<body><h1>Welcome Home</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if(url == '/about'){
+        res.write('<html>');
+        res.write('<head><title>Node JS Response</title></head>');
+        res.write('<body><h1>Welcome to about us page.</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if(url == '/about'){
+        res.write('<html>');
+        res.write('<head><title>Node JS Response</title></head>');
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if(url == '/'){
+        res.write('<html>');
+        res.write('<head><title>Node JS Response</title></head>');
+        res.write('<body><h1>Hello From Node JS</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Node JS Response</title></head>');
+        res.write('<body><h1>Welcome to my Node JS Project</h1></body>');
+        res.write('</html>');
+        res.end();
+})
+server.listen(3000);
